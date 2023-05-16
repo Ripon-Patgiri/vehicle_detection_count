a162728e3eda4cff3e600d0b2724f7039253c339
Ripon-Patgiri <riponmongzeell@gmail.com>
2023-05-16 22:21:38 +0530

    Performed grayscale, gaussial blur, dilate, morph and detected car using car cascade.

--- speed_detector.py
+++ speed_detector.py
@@ -20,3 +20,30 @@ Image.fromarray(image_arr).save('car_image.jpg')
 # convert image to grayscale
 grey = cv2.cvtColor(image_arr, cv2.COLOR_BGR2GRAY)
 Image.fromarray(grey).save('car_image_grayscale.jpg')
+
+# Apply GaussianBlur to remove noise from image
+blur = cv2.GaussianBlur(grey, (5, 5), 0)
+Image.fromarray(blur).save('car_image_gaussiabblur.jpg')
+
+# Dilate the images to fill the missing parts of the images
+dilated = cv2.dilate(blur, np.ones((3, 3)))
+Image.fromarray(dilated).save('car_image_dilation.jpg')
+
+# Perform Morphology Transformation with the kernel
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
+Image.fromarray(closing).save('car_image_morph.jpg')
+
+# Detecting cars using car cascade
+car_cascade_src = 'cars.xml'
+car_cascade = cv2.CascadeClassifier(car_cascade_src)
+cars = car_cascade.detectMultiScale(closing, 1.1, 1)
+cars
+
+# Draw rectange around detected cars
+cnt = 0
+for (x, y, w, h) in cars:
+    cv2.rectangle(image_arr, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cnt += 1
+print(cnt, " cars found")
+Image.fromarray(image_arr)
